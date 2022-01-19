90b6001eab9ce36d762eab59a73774bc2f51b59f
Finn <carstensen.finn99@googlemail.com>
2022-01-19 17:11:28 +0100

    Preprocessing and stop words
    Also remove links, encoded symbols and mentions in preprocessing. Seperate files for texts without stop words (also preprocessed)

--- pipeline/preprocessing.py
+++ pipeline/preprocessing.py
@@ -1,6 +1,9 @@
 import string
 import re
 import pandas as pd
+import nltk
+
+stopwords = nltk.corpus.stopwords.words('english')
 
 def removePunctuation(str):
     return str.translate(str.maketrans('', '', string.punctuation))
@@ -11,7 +14,24 @@ def lower(str):
 def strip(str):
     return re.sub('\s\s+', ' ', str.strip())
 
+def removeMentions(str):
+    return re.sub('@\S+', '', str)
+
+def removeEncodedSymbols(str):
+    return re.sub('&#\d+', '', str)
+
+def removeLinks(str):
+    return re.sub('http:\/\/\S+|https:\/\/\S+', '', str)
+
+def removeStopWords(str):
+    words = str.split(' ')
+    no_stopwords = [e for e in words if e not in stopwords]
+    return ' '.join(no_stopwords)
+
 def processString(str):
+    str = removeMentions(str)
+    str = removeEncodedSymbols(str)
+    str = removeLinks(str)
     str = removePunctuation(str)
     str = lower(str)
     str = strip(str)
@@ -24,4 +44,11 @@ def processStandardCsv(fileNumber):
     df.rename(columns={'Unnamed: 0': 'index'}, inplace=True)
     df.to_csv('datasets/' + fileNumber + '_preprocessed.csv', index=False)
 
-processStandardCsv('6')
+def csvRemoveStopwords(fileNumber):
+    df = pd.read_csv('./datasets/' + fileNumber + '_standard.csv')
+    for idx, item in df.iterrows():
+        df.at[idx, 'text'] = processString(removeStopWords(item['text']))
+    df.to_csv('datasets/' + fileNumber + '_no_stopwords.csv', index=False)
+
+# processStandardCsv('6')
+csvRemoveStopwords('6')
