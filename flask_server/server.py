6692b6b08f71f583bfc9fca1315e58cdda27edba
OliverWands <wandschneider.oliver@gmail.com>
2022-01-25 10:04:43 +0100

    Updated html and server to allow for selection of input.

--- flask_server/server.py
+++ flask_server/server.py
@@ -2,12 +2,17 @@ import numpy as np
 from flask import Flask, render_template, request
 from tensorflow import keras
 
+from reddit_api import RedditWrapper
+
 app = Flask(__name__)
 
 mul_model = keras.models.load_model(
     '/Users/oliverwandschneider/dev/PycharmProjects/hatespeech_classifier/models/multi_class_rnn')
 bin_model = keras.models.load_model(
     '/Users/oliverwandschneider/dev/PycharmProjects/hatespeech_classifier/models/binary_class_rnn')
+
+wrapper = RedditWrapper()
+hs_classes = ['racism', 'religion', 'disability', 'sexism', 'sexual orientation']
 
 
 @app.route('/')
@@ -18,14 +23,30 @@ def index():
 @app.route('/process', methods = ["POST"])
 def process():
     classes = ["No Hatespeech", "Hatespeech"]
-    rawtext = ""
-    pred = []
+    results = list()
     if request.method == 'POST':
+        choice = request.form['taskoption']
         rawtext = request.form['rawtext']
-        pred = bin_model.predict(np.array([rawtext.lower()]))
-        print(pred)
+        rawtext = rawtext.strip()
+        if choice == 'username':
+            values = wrapper.get_user_comments(user_name = rawtext)
+        elif choice == 'posturl':
+            values = wrapper.get_post_comments(link = rawtext)
+        else:
+            values = rawtext.split(sep = "\n")
 
-    return render_template("index.html", results = [classes[np.argmax(pred)]], num_of_results = 1)
+        pred = bin_model.predict(np.array([val.lower() for val in values]))
+        class_pred = mul_model.predict(np.array([val.lower() for val in values]))
+        for inx in range(len(values)):
+            hs_class = ""
+            if np.argmax(pred[inx]) == 1:
+                hs_class = hs_classes[np.argmax(class_pred[inx])]
+            results.append([inx + 1, values[inx], classes[np.argmax(pred[inx])], hs_class])
+
+        return render_template("index.html", results = results, num_of_results = len(results))
+
+    # return render_template("index.html", results = [classes[np.argmax(pred)]], num_of_results = len(results))
+    return render_template("index.html", results = ["Oh no!"], num_of_results = 1)
 
 
 if __name__ == '__main__':
